99d589ef7ca0d7a58c89b03b32f096839e7a9918
Diabl0269 <talefronny@gmail.com>
2020-01-22 13:29:28 +0200

    setup gormatting for amount and createAt

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,18 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import moment from "moment";
+import numeral from "numeral";
 
-const ExpenseListItem = ({
-  id,
-  description,
-  amount,
-  createdAt,
-  note
-}) => (
+const ExpenseListItem = ({ id, description, amount, createdAt, note }) => (
   <div>
     <Link to={`/edit/${id}`}>
       <h3>Description: {description}</h3>
     </Link>
-    Amount: {amount}, Created at: {createdAt} {note && `Note: ${note}`}
+    Amount: {numeral(amount / 100).format("$0,0.00")}, Created at:{" "}
+    {moment(createdAt).format("MMM Do, YYYY")}, Note:
+    {note && `Note: ${note}`}
   </div>
 );
 
