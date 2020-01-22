479324ca47a4d217605554a7156974110c100f21
Diabl0269 <talefronny@gmail.com>
2020-01-22 16:29:26 +0200

    changed ExpensesSummay

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,23 +1,29 @@
 import React from "react";
 import { connect } from "react-redux";
 import selectExpenses from "../selectors/expenses";
-import expensesTotal from '../selectors/expenses-total';
+import selectExpensesTotal from "../selectors/expenses-total";
 import numeral from "numeral";
-
 
 export class ExpensesSummary extends React.Component {
   render() {
-  return <div>
-      Number of Expenses: {this.props.expenses.length} 
-      <br />
-      Total expenses: {numeral(expensesTotal(this.props.expenses) / 100).format("$0,0.00") }
-      </div>;
-  };
-};
+    const formattedExpensesTotal = numeral(
+      this.props.expensesTotal / 100
+    ).format("$0,0.00");
+    return (
+      <div>
+        Number of Expenses: {this.props.expensesCount}
+        <br />
+        Total expenses: {formattedExpensesTotal}
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = state => {
+  const visibleExpenses = selectExpenses(state.expenses, state.filters);
   return {
-    expenses: selectExpenses(state.expenses, state.filters)
+    expensesCount: visibleExpenses.length,
+    expensesTotal: selectExpensesTotal(visibleExpenses)
   };
 };
 
